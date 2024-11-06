8a5b07920ee97d7ffe5a02c27acddac639e017c1
Husen Malee <husenmal@ais.co.th>
2024-11-06 08:50:21 +0000

    Deploy v.2.0.1

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const HOST = '0.0.0.0';
 const app = express();
 
 app.get('/',(req,res) => {
-    res.send('Hello World V.2.0.0 ');
+    res.send('Hello World V.2.0.1');
 });
 
 app.listen(PORT,HOST,() => {
